2450e9915c11d7aa1e037981a23f46b8b910d03c
Juan Isaza <jisazamp@gmail.com>
2021-04-03 12:24:04 -0500

    Limpieza, Navbar y Router

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>Acerca de esta App</h1>
+      <p className='my-1'>
+        Esta es una aplicación full stack para guardar contactos. Realizada con
+        el stack MERN.
+      </p>
+      <p className='bg-dark p'>
+        <strong>Version: 1.0.0</strong>
+      </p>
+    </div>
+  );
+};
+
+export default About;
