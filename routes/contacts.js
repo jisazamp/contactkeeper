ad4a964740c729daea60d240d30cfbc9781b54e8
Juan Isaza <jisazamp@gmail.com>
2021-04-03 11:52:28 -0500

    Actualizar y borrar contactos

--- routes/contacts.js
+++ routes/contacts.js
@@ -63,15 +63,65 @@ router.post(
 // @route   PUT api/contacts/:id
 // @desc    Actualizar contacto
 // @access  Private
-router.put('/:id', (req, res) => {
-  res.send('Actualiza un contacto');
+router.put('/:id', auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  // Construir un objeto de Contact
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  // Para acceder al parámetro de URL, usamos req.params.id
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact)
+      return res.status(404).json({ msg: 'Contacto no encontrado' });
+
+    // Asegurar que el contacto pertenece al usuario logueado
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'No autorizado' });
+    }
+
+    // Actualizar en la BD
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Error de servidor');
+  }
 });
 
 // @route   DELETE api/contacts/:id
 // @desc    Borra un contacto
 // @access  Private
-router.delete('/:id', (req, res) => {
-  res.send('Borra un contacto');
+router.delete('/:id', auth, async (req, res) => {
+  // Desestructurar la info del contacto
+  const { name, email, phone, type } = req.body;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact)
+      return res.status(404).json({ msg: 'Contacto no encontrado' });
+
+    // Asegurar que el contacto pertenece al usuario logueado
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'No autorizado' });
+    }
+
+    // No usar el método Delete, está deprecado
+    await Contact.findByIdAndRemove(req.params.id);
+    res.json({ msg: 'Contacto borrado' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Error de servidor');
+  }
 });
 
 module.exports = router;
