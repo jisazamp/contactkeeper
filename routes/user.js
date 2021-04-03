3cc6991976ffacd160f63e9776ba7a15e98ae5d7
Juan Isaza <jisazamp@gmail.com>
2021-04-02 23:11:38 -0500

    POST de user

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -18,14 +19,39 @@ router.post(
     'password',
     'Por favor ingrese una contraseña de 8 o más caracteres'
   ).isLength({ min: 8 }),
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // 400 significa Bad Request
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send(req.body);
+    const { name, email, password } = req.body;
+    try {
+      // Método de Moongose para hayar una coincidencia
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'El usuario ya existe' });
+      }
+
+      // En este punto no se ha guardado en la BD, solo se ha creado la instancia
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // Cifrado del password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      res.send('El usuario se ha guardado');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Problema de servidor');
+    }
   }
 );
 
