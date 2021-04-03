7328417a8b3d850808d30bcbc3d2812c460de5f9
Juan Isaza <jisazamp@gmail.com>
2021-04-02 23:32:30 -0500

    Ruta de autenticación

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { body, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route   GET api/auth
 // @desc    Devolver un usuario logueado
@@ -11,8 +17,58 @@ router.get('/', (req, res) => {
 // @route   POST api/auth
 // @desc    Autenticar un usuario y devolver el token
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Loguear un usuario');
-});
+router.post(
+  '/',
+  body(
+    'email',
+    'Por favor incluya una dirección de correo electrónico'
+  ).isEmail(),
+  body('password', 'Por favor digite su contraseña').isLength({ min: 5 }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // 400 significa Bad Request
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // Validar credenciales
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: 'Credenciales inválidas' });
+      }
+
+      // Comparar los hashes
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Credenciales inválidas' });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(error.message);
+      res.status(500).send('Error de servidor');
+    }
+  }
+);
 
 module.exports = router;
